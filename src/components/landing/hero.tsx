7cc772d3aac4cbd11715888dc20d011a29b962cb
Maria Eduarda Morais <msmaduhmorais@gmail.com>
2025-08-18 23:56:38 +0000

    atualize todas as minhas mudanças

--- src/components/landing/hero.tsx
+++ src/components/landing/hero.tsx
@@ -8,22 +8,28 @@ export default function Hero() {
       <div className="container mx-auto px-4 md:px-6">
         <div className="grid gap-8 md:grid-cols-2 md:items-center">
           <div className="relative space-y-4 md:space-y-6 z-10 text-center md:text-left">
-            <h1 className="font-main text-3xl font-bold tracking-tight sm:text-4xl md:text-5xl lg:text-4xl xl:text-5xl">
+            <h1 className="font-main text-4xl font-bold tracking-tight sm:text-5xl md:text-5xl lg:text-4xl xl:text-5xl">
               Se você chegou até aqui possivelmente está passando por desafios
             </h1>
             <p className="text-lg md:text-xl text-foreground/80">
               Estou aqui para te auxiliar nesta jornada!
             </p>
           </div>
-          <div className="relative mt-8 flex items-center justify-center md:mt-0">
-            <div className="relative w-full max-w-sm md:max-w-md">
+          <div className="relative mt-8 flex h-[350px] items-center justify-center md:mt-0 md:h-[500px]">
+            <div className="relative h-full w-full max-w-sm md:max-w-md group">
+              <div 
+                className="absolute inset-0 bg-[#EAF5A4] opacity-50 rounded-[50%_50%_50%_50%/50%_50%_50%_50%] transform -rotate-12"
+              />
               <Image
                 alt="Psicóloga Bruna Morais Miranda"
-                src="/images/home.png"
-                width={600}
-                height={860}
-                className="h-auto w-full rounded-lg object-cover shadow-lg"
+                src="/images/hero.png"
+                fill
+                className="absolute inset-0 h-full w-full object-contain z-10"
                 priority
+                style={{
+                  clipPath:
+                    'polygon(15% 0, 100% 10%, 100% 85%, 85% 100%, 10% 90%, 0 25%)',
+                }}
               />
             </div>
           </div>
